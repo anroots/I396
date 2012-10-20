80a5a51a8f295853a57a1e9d023653983f4b3415
Janno Tomingas <janno.tomingas@gmail.com>
2012-10-20 23:43:07 +0300

    2. ülesande algus (size, add, remove ja contains peaksid olema kaetud), containsAll esimene test olemas.

--- src/ee/itcollege/i396/ex2/LinkedSet.java
+++ src/ee/itcollege/i396/ex2/LinkedSet.java
@@ -1,5 +1,86 @@
 package ee.itcollege.i396.ex2;
 
-public class LinkedSet {
+import java.util.List;
 
+public class LinkedSet {
+	
+	private Node head;
+	private Node tail;
+	
+	public LinkedSet() {
+		head = tail = new Node(null, null, null);
+	}
+	
+	int size() {
+		int size = 0;
+		Node run = head.next;
+		
+		while (run != null) {
+			size++;
+			run = run.next;
+		}
+		
+		return size;
+	}
+	
+	public boolean contains(Object o) {
+		Node run = head.next;
+		
+		while (run != null) {
+			if (run.data.equals(o)) {
+				return true;
+			}
+			run = run.next;
+		}
+		return false;
+	}
+	
+	public void add(Object o) {
+		if (this.contains(o)) {
+			return;
+		}
+		tail.next = new Node(o, this.tail, null);
+		tail.next.previous = tail;
+		tail = tail.next;
+	}
+	
+	public void remove(Object o) {
+		Node run = head.next;
+		if (run == null) {
+			throw new IllegalStateException();
+		}
+		
+		while (run != null) {
+			if (run.data.equals(o)) {
+				run.previous.next = run.next;
+				if (run.next != null) {
+					run.next.previous = run.previous;	
+				}
+				return;
+			}
+			run = run.next;
+		}
+	}
+	
+	public Boolean containsAll(List<Object> objs) {
+		for (Object o : objs) {
+			if (!this.contains(o)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private class Node
+	{
+		private Object data;
+		private Node next;
+		private Node previous;
+
+		public Node(Object data, Node previous, Node next) {
+			this.data = data;
+			this.previous = previous;
+			this.next = next;
+		}
+	}
 }
--- src/ee/itcollege/i396/ex2/LinkedSetTest.java
+++ src/ee/itcollege/i396/ex2/LinkedSetTest.java
@@ -1,14 +1,90 @@
 package ee.itcollege.i396.ex2;
 
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
 public class LinkedSetTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createSizeIsZero() {
+		LinkedSet ls = new LinkedSet();
+		
+		assertThat(ls.size(), is(0));
+	}
+	
+	@Test
+	public void newContainsIsFalse() {
+		LinkedSet ls = new LinkedSet();
+		
+		assertThat(ls.contains(1), is(false));
+	}
+	
+	@Test
+	public void addSizeIsCorrect() {
+		LinkedSet ls = new LinkedSet();
+		ls.add(1);
+		
+		assertThat(ls.size(), is(1));
+	}
+	
+	@Test
+	public void containsIsCorrect() {
+		LinkedSet ls = new LinkedSet();
+		ls.add(1);
+		
+		assertThat(ls.contains(1), is(true));
+	}
+	
+	@Test
+	public void addDuplicateSizeIsCorrect() {
+		LinkedSet ls = new LinkedSet();
+		ls.add(1);
+		ls.add(1);
+		
+		assertThat(ls.size(), is(1));
+	}
+	
+	@Test
+	public void removeLastSizeIsCorrect() {
+		LinkedSet ls = new LinkedSet();
+		ls.add(1);
+		ls.remove(1);
+		
+		assertThat(ls.size(), is(0));
+	}
+	
+	@Test
+	public void removeSizeIsCorrect() {
+		LinkedSet ls = new LinkedSet();
+		ls.add(1);
+		ls.add(2);
+		ls.remove(1);
+		
+		assertThat(ls.size(), is(1));
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void emptyRemoveThrows() {
+		LinkedSet ls = new LinkedSet();
+		ls.remove(1);
 	}
 
+	// Nonexistent throws test?
+	
+	@Test
+	public void newContainsAllIsFalse() {
+		LinkedSet ls = new LinkedSet();
+		ArrayList<Object> list = new ArrayList<Object>();
+		list.add(1);
+		
+		assertThat(ls.containsAll(list), is(false));
+	}
+	
 }
