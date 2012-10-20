d3ce90fab8394a751afdc8d3e52b5a8ad9cbb9b8
Janno Tomingas <janno.tomingas@gmail.com>
2012-10-20 22:09:56 +0300

    1. ülesande testid. Tundub okei, vajab värske silmaga üle vaatamist. 1.2 osa testid tabisin vastvalt funktsioonisisesele paigutusele.

--- src/ee/itcollege/i396/ex1/Ex1Test.java
+++ src/ee/itcollege/i396/ex1/Ex1Test.java
@@ -8,11 +8,28 @@ public class Ex1Test {
 
 	@Test
 	public void testCoverage1() {
-		fail("Not yet implemented");
+		Ex1 ex = new Ex1();
+		
+		assertEquals(ex.coverage1(1, 0), 1);
+		assertEquals(ex.coverage1(1, 2), 2);
+		assertEquals(ex.coverage1(1, 3), 2);
+		
+		assertEquals(ex.coverage1(2, 0), 2);
+		assertEquals(ex.coverage1(2, 3), 2);
+		
+		assertEquals(ex.coverage1(0, 0), 2);
+		assertEquals(ex.coverage1(0, 4), 2);
 	}
 
 	@Test
 	public void testCoverage2() {
-		fail("Not yet implemented");
+		Ex1 ex = new Ex1();
+		
+		assertEquals(ex.coverage2(new int[] {}, 0), 1);
+			assertEquals(ex.coverage2(new int[] {1}, 0), 2);
+				assertEquals(ex.coverage2(new int[] {0, 1}, 2), 3);	
+				
+				assertEquals(ex.coverage2(new int[] {0, 2}, 1), 1);
+					assertEquals(ex.coverage2(new int[] {0, 1, 2}, 1), 3);
 	}
 }
